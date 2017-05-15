36bdf9fc39d000bc29280e280ff0404b1adb0ec5
DESKTOP-M8MLKKL\廖冠凱 <fay19931224@gmail.com>
2017-05-15 20:36:34 +0800

    enable connected component

--- NightCarDetected/main.cpp
+++ NightCarDetected/main.cpp
@@ -11,17 +11,14 @@ using namespace std;
 using namespace cv;
 
 
-void removeNoice(Mat binaryImg) {
+/*void removeNoice(Mat binaryImg) {
 	Mat erodeStruct = getStructuringElement(MORPH_RECT, Size(5, 5));
 	erode(binaryImg, binaryImg, erodeStruct);
-
 	imshow("Erode", binaryImg);
+}*/
 
-}
-
-
-/*void detectLight(Mat srcImg, Mat binaryImg, int offsetX, int offsetY) {
-	int threshold = 100;
+void detectLight(Mat srcImg, Mat binaryImg, int offsetX, int offsetY) {
+	
 	Mat labelImg, stats, centroids;
 	int nLabels = connectedComponentsWithStats(binaryImg, labelImg, stats, centroids, 8, CV_32S);
 
@@ -30,36 +27,36 @@ void removeNoice(Mat binaryImg) {
 	colors[0] = cv::Vec3b(0, 0, 0);
 	//std::cout << "Number of connected components = " << nLabels << std::endl << std::endl;
 
-	for (int label = 1; label < nLabels; ++label) {
-		colors[label] = cv::Vec3b((std::rand() & 255), (std::rand() & 255), (std::rand() & 255));
-
-		int left = stats.at<int>(label, CC_STAT_LEFT) + offsetX;
-		int top = stats.at<int>(label, CC_STAT_TOP) + offsetY;
+	for (int label = 1; label < nLabels; ++label) 
+	{		
 		int width = stats.at<int>(label, CC_STAT_WIDTH);
 		int height = stats.at<int>(label, CC_STAT_HEIGHT);
 		int area = stats.at<int>(label, CC_STAT_AREA);
 		Point centroid = Point(centroids.at<double>(label, 0), centroids.at<double>(label, 1));
 
-		if (area > 100 && abs(width / height) < 2) {
-			rectangle(srcImg, Rect(left, top, width, height), Scalar(0, 0, 255), 2);
-
+		if (area > 100 && (binaryImg.rows/80<height) && (binaryImg.rows / 4>height) && (binaryImg.cols / 60<width) && (binaryImg.cols / 4>width)&&(width>height))
+		{
+			colors[label] = cv::Vec3b((std::rand() & 255), (std::rand() & 255), (std::rand() & 255));
+			int left = stats.at<int>(label, CC_STAT_LEFT) + offsetX;
+			int top = stats.at<int>(label, CC_STAT_TOP) + offsetY;
+			rectangle(srcImg, Rect(left, top, width, height), Scalar(255, 255, 255), 2);
 		}
 
 
 	}
 
 	//show labeling color
-	Mat dst(binaryImg.size(), CV_8UC3);
+	/*Mat dst(binaryImg.size(), CV_8UC3);
 	for (int r = 0; r < dst.rows; ++r) {
 		for (int c = 0; c < dst.cols; ++c) {
 			int label = labelImg.at<int>(r, c);
 			Vec3b &pixel = dst.at<Vec3b>(r, c); //get the index of the pixel
 			pixel = colors[label]; //set new pixel
 		}
-	}
-	imshow("Labeling", dst);
+	}*/
+	//imshow("Labeling", dst);
 }
-*/
+
 
 int thresholdValue(Mat& src)
 {
@@ -83,24 +80,57 @@ int thresholdValue(Mat& src)
 	for (int i = 0; i < 256; i++)
 	{
 		sumOfGrayLevel += grayLevel[i];		
-		if (sumOfGrayLevel>NumberOfPixel*0.95){
-			//cout << i << endl;
+		if (sumOfGrayLevel>NumberOfPixel*0.99)
+		{
+			cout << i << endl;
 			return i;
 		}
 	}		
 }
 
-int main() {	
+Mat detectLight(Mat src, Rect rect)
+{
+	Mat ROI,ROITemp;
+	
+	ROITemp = src(rect);
+	ROI = ROITemp.clone();
+
 
 	int previousThresholdValue = NULL;
 	int ThresholdValue = 0;
 	int ThresholdValueAdjust = 0;
+	ThresholdValue = thresholdValue(ROI);
+	if (previousThresholdValue == NULL)
+	{
+		previousThresholdValue = ThresholdValue;
+	}
+	ThresholdValueAdjust = ThresholdValue*0.875 + previousThresholdValue*0.125;
+	threshold(ROI, ROI, ThresholdValueAdjust, 255, THRESH_BINARY); //OTSU is not necessary to set thres
+	previousThresholdValue = ThresholdValueAdjust;
+	Mat kernal()
+	erode(ROI, ROI, 2);
+	dilate(ROI, ROI, 3);
+
+	//removeNoice(rightROI);
+	//detectLight(rightSrc, rightROI, 100, rightSrc.rows / 7 * 2);
+
+	//imshow("Left Src", leftSrc);
+	//imshow("Left Grey", leftGrey);
+	//imshow("Left ROI", leftROI);
+	
+	return ROI;
+}
+
+int main() {	
 	Mat src;
-	Mat leftSrc, rightSrc;
-	Mat leftROI, rightROI, rightROITemp;
-	Mat leftGrey, rightGrey;	
-	string path = "C://Users//¹ù«a³Í//Desktop//NightVehicleDetection//NightVehicleDetection//AV1-20170510_213352.avi";
-	//string path = "C://Users//¹ù«a³Í//Desktop//allvideos//car1.avi";
+	Mat rightSrc, leftSrc;
+	Mat rightDst, leftDst;	
+	Mat leftGray, rightGray;
+	
+	//string path = "C://Users//¹ù«a³Í//Desktop//NightVehicleDetection//NightVehicleDetection//AV1-20170510_213352.avi";//µLÂo
+	string path = "C://Users//¹ù«a³Í//Desktop//Produce.mp4";//Âo
+	
+
 
 	VideoCapture capture(path);
 	if (!capture.isOpened()) {
@@ -110,38 +140,35 @@ int main() {
 
 	Size videoSize = Size((int)capture.get(CV_CAP_PROP_FRAME_WIDTH), (int)capture.get(CV_CAP_PROP_FRAME_HEIGHT));	
 
-	while (true) {
+	while (true)
+	{
 		capture >> src;
 		if (src.empty()) {
 			break;
 		}
 
-		rightSrc = src(Rect(videoSize.width / 2, videoSize.height / 2, videoSize.width / 2, videoSize.height / 2));
-		cvtColor(rightSrc, rightGrey, CV_BGR2GRAY);
-		Rect ROI = Rect(100, rightGrey.rows / 7 * 2, rightGrey.cols / 7 * 6, rightGrey.rows / 4 * 2);				
-		rightROITemp = rightGrey(ROI);
-		rightROI=rightROITemp.clone();
-				
-		ThresholdValue = thresholdValue(rightROI);
-		if (previousThresholdValue == NULL)
-		{
-			previousThresholdValue = ThresholdValue;
-		}
-		ThresholdValueAdjust = ThresholdValue*0.875 + previousThresholdValue*0.125;
-		threshold(rightROI, rightROI, ThresholdValueAdjust, 255, THRESH_BINARY); //OTSU is not necessary to set thres
-		previousThresholdValue = ThresholdValueAdjust;
-		erode(rightROI, rightROI,3);
+
+	/*	leftSrc = src(Rect(videoSize.width / 2, videoSize.height / 2, videoSize.width / 2, videoSize.height / 2));
+		cvtColor(leftSrc, leftGray, CV_BGR2GRAY);
+		Rect leftRect = Rect(100, leftGray.rows / 7 * 2, leftGray.cols / 7 * 6, leftGray.rows / 4 * 2);
+		leftDst=detectLight(leftGray, leftRect);
+		*/
 		
-		//removeNoice(rightROI);
-		//detectLight(rightSrc, rightROI, 100, rightSrc.rows / 7 * 2);
+		rightSrc = src(Rect(0, videoSize.height / 2, videoSize.width / 2, videoSize.height / 2));
+		cvtColor(rightSrc, rightGray, CV_BGR2GRAY);
+		Rect rightRect = Rect(0, rightGray.rows / 32*9, rightGray.cols/5*4 , rightGray.rows /5 * 2);
+		rectangle(rightGray,rightRect,Scalar(0,0,255),5,8,0);
+		rightDst = detectLight(rightGray, rightRect);
 
-		//imshow("Left Src", leftSrc);
-		//imshow("Left Grey", leftGrey);
-		//imshow("Left ROI", leftROI);
 
-		imshow("Right Src", rightSrc);
-		imshow("Right Grey", rightGrey);
-		imshow("Right ROI", rightROI);
+		detectLight(rightGray, rightDst, 0, rightGray.rows / 32 * 9);
+		//imshow("Src", src);
+		/*imshow("Left Grey", leftGray);
+		imshow("Left ROI", leftDst);*/
+
+		
+		imshow("Right Grey", rightGray);
+		imshow("Right ROI", rightDst);
 
 		waitKey(1);
 	}
